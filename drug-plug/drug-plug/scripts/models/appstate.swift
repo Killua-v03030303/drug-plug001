53362d5f3616bdbd2cc4d418ffa71c527b9eb17f
Killua-v03030303 <Aneta.skrobek1987@wp.pl>
2025-08-31 21:25:22 +0200

    Add files via upload

---
+++ drug-plug/drug-plug/scripts/models/appstate.swift
@@ -0,0 +1,22 @@
+//
+//  appstate.swift
+//  drug-plug
+//
+//  Created by Morris Romagnoli on 28/08/2025.
+//
+import SwiftUI
+import Combine
+
+class AppState: ObservableObject {
+    @Published var isSessionActive = false
+    @Published var currentView: ViewType = .timer
+    @Published var showingSettings = false
+    
+    enum ViewType {
+        case timer, stats, settings, music
+    }
+    
+    init() {
+        // Initialize app state
+    }
+}
